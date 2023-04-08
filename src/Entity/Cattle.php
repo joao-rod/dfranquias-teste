3a8db21a0eb47fa125d36dba1a7d40b9545f1455
joao <jvr2564@gmail.com>
2023-04-07 21:04:02 -0300

    chore: create db

---
+++ src/Entity/Cattle.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CattleRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CattleRepository::class)]
+class Cattle
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::BIGINT)]
+    private ?string $cod = null;
+
+    #[ORM\Column]
+    private ?float $milk = null;
+
+    #[ORM\Column]
+    private ?float $week_portion = null;
+
+    #[ORM\Column]
+    private ?float $cattle_weight = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $birth = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCod(): ?string
+    {
+        return $this->cod;
+    }
+
+    public function setCod(string $cod): self
+    {
+        $this->cod = $cod;
+
+        return $this;
+    }
+
+    public function getMilk(): ?float
+    {
+        return $this->milk;
+    }
+
+    public function setMilk(float $milk): self
+    {
+        $this->milk = $milk;
+
+        return $this;
+    }
+
+    public function getWeekPortion(): ?float
+    {
+        return $this->week_portion;
+    }
+
+    public function setWeekPortion(float $week_portion): self
+    {
+        $this->week_portion = $week_portion;
+
+        return $this;
+    }
+
+    public function getCattleWeight(): ?float
+    {
+        return $this->cattle_weight;
+    }
+
+    public function setCattleWeight(float $cattle_weight): self
+    {
+        $this->cattle_weight = $cattle_weight;
+
+        return $this;
+    }
+
+    public function getBirth(): ?\DateTimeInterface
+    {
+        return $this->birth;
+    }
+
+    public function setBirth(\DateTimeInterface $birth): self
+    {
+        $this->birth = $birth;
+
+        return $this;
+    }
+}
---
+++ src/Repository/CattleRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Cattle;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Cattle>
+ *
+ * @method Cattle|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Cattle|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Cattle[]    findAll()
+ * @method Cattle[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CattleRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Cattle::class);
+    }
+
+    public function save(Cattle $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Cattle $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return Cattle[] Returns an array of Cattle objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('c.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Cattle
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
