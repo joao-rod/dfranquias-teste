b294ac6ed2c7228fa7f066bb0c6cca4fbbf70a90
joao <jvr2564@gmail.com>
2023-04-11 01:16:48 -0300

    style: add bootstrap offline and adjust layout

--- src/Controller/CattleController.php
+++ src/Controller/CattleController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Cattle;
+use App\Form\CattleType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,8 +15,21 @@ class CattleController extends AbstractController
     #[Route('/cattle', name: 'homepage')]
     public function index(): Response
     {
-        return $this->render('cattle/index.html.twig', [
-            'controller_name' => 'CattleController',
-        ]);
+        return $this->render('cattle/index.html.twig');
+    }
+
+    #[Route('/cattle/create', name: 'new_cattle')]
+    public function create(EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(CattleType::class);
+        $data['form'] = $form;
+
+        // $cattle = new Cattle;
+        // $cattle->setCod(293);
+
+        // $em->persist($cattle);
+        // $em->flush();
+
+        return $this->renderForm('cattle/create.html.twig', $data);
     }
 }
---
+++ src/Form/CattleType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Cattle;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class CattleType extends AbstractType {
+  public function buildForm(FormBuilderInterface $builder, array $options)
+  {
+    $builder
+      ->add('cod', IntegerType::class, ['label' => 'Código do animal'])
+      ->add('milk', NumberType::class, ['label' => 'Litros de leite por semana'])
+      ->add('week_portion', NumberType::class, ['label' => 'Ração em kg por semana'])
+      ->add('cattle_weight', NumberType::class, ['label' => 'Peso do animal'])
+      ->add('birth', BirthdayType::class, [
+        'format' => 'dd MMMM yyyy',
+        'placeholder' => [
+          'day' => 'Selecione o dia',
+          'month' => 'Selecione o mês',
+          'year' => 'Selecione o ano',
+        ],
+        'label' => 'Data de nascimento'
+      ]);
+  }
+
+  // Configura a label para o nome do primeiro parametro do método add acima
+  // public function configureOptions(OptionsResolver $resolver)
+  // {
+  //   $resolver->setDefaults([
+  //     'data_class' => Cattle::class
+  //   ]);
+  // }
+}
